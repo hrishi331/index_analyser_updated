f9f365622aa65e3c51e04440c3746b5b739dd54f
Hrishikesh Kulkarni <hrishikesh331@gmail.com>
2025-08-24 19:48:45 +0530

    Update 1_App.py
    experimenting

--- pages/1_App.py
+++ pages/1_App.py
@@ -89,9 +89,9 @@ current_price = df[d[olhc]].iloc[-1]
 st.write(current_price)
 
 if olhc == 'Low':
-    data = data[data[d[olhc]]<current_price]
+    data = data[data['Low']<current_price]
 elif olhc == 'High':
-    data = data[data[d[olhc]]>current_price]
+    data = data[data['High']>current_price]
 else :
     pass
 
